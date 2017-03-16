d1ddb40582d93fdfd42c3d2682222038b861c396
robin <462021522@qq.com>
2017-03-16 12:18:17 +0800

    '显示首页'

--- index.android.js
+++ index.android.js
@@ -6,61 +6,23 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Image,
-  Text,
-  View,
-  Navigator
+    Navigator,
+    AppRegistry
 } from 'react-native';
-import ListViewText from './ListViewText';
-import FecthText from './FecthText';
+import WelcomePage from './js/page/WelcomePage';
 
-export default class git_hub extends Component {
-  // 构造
-    constructor(props) {
-      super(props);
-      // 初始状态
-      this.state = {
-
-      };
-    }
-  render() {
-    return (
-        <View style={styles.container}>
-            <Navigator
-                initialRoute={
-                    {
-                        component:FecthText
-                    }
-                }
-                renderScene={(route,navigator)=>{
-                    let Component=route.component;
-                    return <Component  navigator={navigator} {...route.params} />
-                }}
-            ></Navigator>
-        </View>
-    );
+export default class SetUp extends Component{
+  renderScene(route, navigator){
+    let Component=route.component;
+    return <Component {...route.params} navigator={navigator} />
+  }
+  render(){
+    return <Navigator
+        initialRoute={{component:WelcomePage}}
+        renderScene={(route,navigator)=>this.renderScene(route,navigator)}
+    />
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  page1:{
-    flex:1,
-    backgroundColor:'green'
-  },
-  page2:{
-    flex:1,
-    backgroundColor:'yellow'
-  },
-  img:{
-    height:22,
-    width:22
-  }
-});
+AppRegistry.registerComponent('git_hub', () => SetUp);
 
-AppRegistry.registerComponent('git_hub', () => git_hub);
---
+++ js/page/WelcomePage.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/3/16 0016.
+ */
+import React, { Component } from 'react';
+import {
+    Text,
+    View
+} from 'react-native';
+
+
+export default class WelcomePage extends Component{
+
+    render(){
+        return <View>
+            <Text>welcome</Text>
+        </View>
+    }
+}
